29ffbb9a705c311bee0650fa63d9c5c9e4522788
Michael Roitzsch <reactorcontrol@icloud.com>
2021-07-09 09:43:38 +0200

    converter: add class-based DVDReader interface
    public DVDReader class representing XPC-side libdvdread state

--- Importers/DVDImporter.swift
+++ Importers/DVDImporter.swift
@@ -1,8 +1,11 @@
 import Foundation
 import MovieArchiveModel
+import MovieArchiveConverter
 
 
 struct DVDImporter: ImportPass {
 
-	public init(fromSource: URL) throws {}
+	public init(fromSource url: URL) throws {
+		let _ = try DVDReader(fromSource: url)
+	}
 }
